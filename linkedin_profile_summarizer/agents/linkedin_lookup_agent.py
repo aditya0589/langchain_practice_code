74fcf4b9c970332c50b7e81e1ea03e86beb4fe63
GradientBooster <144033691+aditya0589@users.noreply.github.com>
2025-06-28 21:32:03 +0530

    Update linkedin_lookup_agent.py

--- linkedin_profile_summarizer/agents/linkedin_lookup_agent.py
+++ linkedin_profile_summarizer/agents/linkedin_lookup_agent.py
@@ -11,3 +11,24 @@ from langchain import hub
 
 load_dotenv()
 
+def lookup(name: str) -> str:
+    llm = ChatGroq(temperature = 0, model='llama3-70b-8192')
+
+    template = """
+    given the full name of the person {full_name_person}, i want you
+    to get me a link to their linkedin profile, your answer should only
+    contain a URL
+    """
+
+    prompt_template = PromptTemplate(
+        template=template, input_variables=['full_name_person']
+    )
+    tools_for_agent = [
+        Tool("Crawl Google 4 linkedin profile page",
+             func = "?",
+             description="useful for when you want to get the Linkedin Page URL"
+             )
+    ]
+    react_prompt = hub.pull('hwchase17/react') # Harrsion chase 's react agent
+
+    
