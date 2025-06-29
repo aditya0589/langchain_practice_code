9d7efaf26051af52cc83a4d3beb538336f4df59f
GradientBooster <144033691+aditya0589@users.noreply.github.com>
2025-06-29 20:10:38 +0530

    Update linkedin_lookup_agent.py

--- linkedin_profile_summarizer/agents/linkedin_lookup_agent.py
+++ linkedin_profile_summarizer/agents/linkedin_lookup_agent.py
@@ -7,21 +7,27 @@ from langchain.agents import (
     AgentExecutor
 )
 from langchain import hub
+import sys
+import os
+
+# Dynamically add project root to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from linkedin_profile_summarizer.tools.tools import get_profile_url_tavily
 
 
 load_dotenv()
 
 def lookup(name: str) -> str:
-    llm = ChatGroq(temperature = 0, model='llama3-70b-8192')
+    llm = ChatGroq(temperature = 0, model='llama3-70b-8192', groq_api_key=os.environ["GROQ_API_KEY"])
 
     template = """
-    given the full name of the person {full_name_person}, i want you
+    given the full name of the person {name_of_person}, i want you
     to get me a link to their linkedin profile, your answer should only
     contain a URL
     """
 
     prompt_template = PromptTemplate(
-        template=template, input_variables=['full_name_person']
+        template=template, input_variables=['name_of_person']
     )
     tools_for_agent = [
         Tool("Crawl Google 4 linkedin profile page",
@@ -30,7 +36,7 @@ def lookup(name: str) -> str:
              )
     ]
     react_prompt = hub.pull('hwchase17/react') # Harrsion chase 's react agent
-    agent = create_react_agent(llm=llm, toole=tools_for_agent, prompt=react_prompt)
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
     agent_executer=AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
 
     result = agent_executer.invoke(
